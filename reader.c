0928eef068bd468e2590f5836372dff5255c2cf4
omiguelgomes <miguelrgomes1698@gmail.com>
2020-10-20 09:49:15 +0100

    Added initial functions specification

---
+++ reader.c
@@ -0,0 +1,18 @@
+int main()
+{
+    return 1;
+}
+
+
+/*
+argumentos
+– fd: identificador da ligação de dados
+– buffer: array de caracteres recebidos
+*
+retorno
+– comprimento do array (número de caracteres lidos)
+– valor negativo em caso de erro*/
+int llread(int fd, char * buffer)
+{
+    return 1;
+}
