7845e660c122d3cf8a72fc072ed3a7303bcc2793
omiguelgomes <miguelrgomes1698@gmail.com>
2020-10-20 10:21:38 +0100

    Added class1 code to project files

--- writer.c
+++ writer.c
@@ -1,9 +1,123 @@
+/*Non-Canonical Input Processing*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
 #include "writer.h"
 
-int main()
+
+#include <strings.h>
+#include <string.h>
+#include <stdlib.h> //for exit
+#include <fcntl.h> // for open
+#include <unistd.h> // for close
+
+#define BAUDRATE B38400
+#define MODEMDEVICE "/dev/ttyS1"
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+
+volatile int STOP=FALSE;
+
+int main(int argc, char** argv)
 {
-    return 1;
+    int fd,c, res;
+    struct termios oldtio,newtio;
+    char buf[255];
+    int i, sum = 0, speed = 0;
+    
+    if ( (argc < 2) || 
+  	     ((strcmp("/dev/ttyS10", argv[1])!=0) && 
+  	      (strcmp("/dev/ttyS11", argv[1])!=0) )) {
+      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS10\n");
+      exit(1);
+    }
+
+
+  /*
+    Open serial port device for reading and writing and not as controlling tty
+    because we don't want to get killed if linenoise sends CTRL-C.
+  */
+
+    fd = open(argv[1], O_RDWR | O_NOCTTY );
+    if (fd <0) {perror(argv[1]); exit(-1); }
+
+    printf("hi\n");
+    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+      printf(argv[1], "%d");
+      perror("tcgetattr");
+      exit(-1);
+    }
+
+    bzero(&newtio, sizeof(newtio));
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    /* set input mode (non-canonical, no echo,...) */
+    newtio.c_lflag = 0;
+
+    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+
+
+
+  /* 
+    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
+    leitura do(s) pr�ximo(s) caracter(es)
+  */
+
+
+
+    tcflush(fd, TCIOFLUSH);
+
+    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
+
+    printf("New termios structure set\n");
+    printf("Enter a line to be transmitted: \n");
+
+    //recomended function(gets) is dangerous and deprecated
+    fgets(buf, 255, stdin);
+
+
+    //Create new buffer with exact size of string received, no trash
+    int counter = 0;
+    while(buf[counter] != '\0')
+    {
+      counter++;
+    }
+
+    int bufferSize = counter;
+    counter = 0;
+    char newBuffer[bufferSize + 1]; //acount for '\0' in the end
+
+    while(counter < bufferSize)
+    {
+      newBuffer[counter] = buf[counter];
+      counter++;
+    }
+    newBuffer[counter] = '\0';
+
+    //done with the new buffer
+
+    res = write(fd, newBuffer, bufferSize);
+    printf("%d bytes written\n", res);
+
+    if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
+
+    close(fd);
+    return 0;
 }
+
 
 int llwrite(int fd, char * buffer, int length)
 {
