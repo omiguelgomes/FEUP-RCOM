545a103a5b8713257785bc513cf0ac1ba5c71834
omiguelgomes <miguelrgomes1698@gmail.com>
2020-10-20 10:51:27 +0100

    Added more flag macros

--- reader.h
+++ reader.h
@@ -1,5 +1,7 @@
 //byte(A)
 
+    /*TRAMA INFORMACAO*/
+
 //address field byte for command sent by sender,
 //or answer by receiver
 #define ADDRESS_FIELD_SENDER 0x03
@@ -7,6 +9,17 @@
 //address field byte for command sent by receiver,
 //or answer by sender
 #define ADDRESS_FIELD_RECEIVER 0x01
+
+//check slides, didnt understand
+#define I_CONTROL_FIELD 0x01
+
+
+
+    /*TRAMA SUPERVISAO E NAO NUMERADO*/
+#define S-U_CONTROL_FIELD_SET 0x03
+#define S-U_CONTROL_FIELD_DISC 0x0B
+#define S-U_CONTROL_FIELD_UA 0x07
+
 
 /*-------------------------FUNCTIONS-------------------------*/
 int main();
